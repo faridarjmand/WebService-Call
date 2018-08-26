dce697f14d385537e50ac2c81f1e4cab0df8b230
Farid Arjmand <faridarjmand73@gmail.com>
2018-08-26 16:45:56 +0430

    Update suds.py

--- suds.py
+++ suds.py
@@ -1,9 +1,18 @@
 from suds.client import Client
-url="http://wsf.cdyne.com/WeatherWS/Weather.asmx?WSDL"
-client = Client(url)
-print client ## shows the details of this service
+# The service URL
+soap_url = 'http://myapp.example.notreal/path/to/soap'
+# The WSDL URL, we wont' use this but just illustrating for example. This 
+# would be the file you download to your system and save as wsdl_file
+wsdl_url = 'http://myapp.example.notreal/path/to/soap?wsdl' 
+# The full path to the downloaded WSDL file on your local system
+wsdl_file = '/path/to/myapp.wsdl'
+wsdl_url = 'file://' + wsdl_file # Override original wsdl_url
+
+client = Client(url=wsdl_url, location=soap_url)
+#client = Client(wsdl_url)
+
+# shows the details of this service
+print client
 
 result = client.service.GetWeatherInformation() 
 print result
-
-#sudo tcpdump -As 0 
